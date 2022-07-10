ef6a2334b816200d8de6636fb2ac52dd1aa6ce35
Yugo Sugawara <93136879+ysuga223@users.noreply.github.com>
2022-07-10 15:15:03 +0900

    Initial commit

---
+++ src/components/ChildArea.jsx
@@ -0,0 +1,27 @@
+import { memo } from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  const data = [...Array(2000).keys()];
+  console.log("ChildAreaがレンダリングされた！");
+  data.forEach(() => {
+    console.log("...");
+  });
+
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
